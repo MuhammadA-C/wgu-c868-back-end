c719711aa3aa0417c21f404d491e8e4fe9086699
MuhammadA-C <phantomganhdi@gmail.com>
2023-12-18 23:21:15 -0800

    Refactored

---
+++ back-end/controllers/menuItemController.js
@@ -0,0 +1,66 @@
+const MenuItemDAOImpl = require("../dao/MenuItemDAOImpl");
+
+exports.createMenuItem = (req, res) => {
+  //const { name, description, picture, price } = req.body;
+
+  /*
+      Need database call
+    */
+  res.status(201).json({
+    status: "success",
+    data: { test: "test" },
+  });
+};
+
+exports.getAllMenuItems = (req, res) => {
+  MenuItemDAOImpl.fetchAll()
+    .then(([rows, fieldData]) => {
+      if (rows.length == 0) {
+        res.status(200).json({
+          status: "success",
+          results: rows.length,
+          message: "Database has no data for the MenuItems table",
+        });
+        return;
+      }
+
+      res.status(200).json({
+        status: "success",
+        results: rows.length,
+        data: rows,
+      });
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
+exports.getMenuItemByID = (req, res) => {
+  /*
+      Need database call
+    */
+  res.status(200).json({
+    status: "success",
+    data: { test: "test" },
+  });
+};
+
+exports.updateMenuItemByID = (req, res) => {
+  /*
+      Need database call
+  */
+  res.status(200).json({
+    status: "success",
+    data: { test: "test" },
+  });
+};
+
+exports.deleteMenuItemByID = (req, res) => {
+  /*
+      Need database call
+  */
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+};
--- back-end/routers/menuItemRouter.js
+++ back-end/routers/menuItemRouter.js
@@ -1,78 +1,16 @@
 const express = require("express");
-const MenuItemDAOImpl = require("../dao/MenuItemDAOImpl");
 const router = express.Router();
+const menuItemController = require("../controllers/menuItemController");
 
-const createMenuItem = (req, res) => {
-  //const { name, description, picture, price } = req.body;
-
-  /*
-      Need database call
-    */
-  res.status(201).json({
-    status: "success",
-    data: { test: "test" },
-  });
-};
-
-const getAllMenuItems = (req, res) => {
-  MenuItemDAOImpl.fetchAll()
-    .then(([rows, fieldData]) => {
-      if (rows.length == 0) {
-        res.status(200).json({
-          status: "success",
-          results: rows.length,
-          message: "Database has no data for the MenuItems table",
-        });
-        return;
-      }
-
-      res.status(200).json({
-        status: "success",
-        results: rows.length,
-        data: rows,
-      });
-    })
-    .catch((err) => {
-      throw err;
-    });
-};
-
-const getMenuItemByID = (req, res) => {
-  /*
-      Need database call
-    */
-  res.status(200).json({
-    status: "success",
-    data: { test: "test" },
-  });
-};
-
-const updateMenuItemByID = (req, res) => {
-  /*
-      Need database call
-  */
-  res.status(200).json({
-    status: "success",
-    data: { test: "test" },
-  });
-};
-
-const deleteMenuItemByID = (req, res) => {
-  /*
-      Need database call
-  */
-  res.status(204).json({
-    status: "success",
-    data: null,
-  });
-};
-
-router.route("/").get(getAllMenuItems).post(createMenuItem);
+router
+  .route("/")
+  .get(menuItemController.getAllMenuItems)
+  .post(menuItemController.createMenuItem);
 
 router
   .route("/:id")
-  .get(getMenuItemByID)
-  .patch(updateMenuItemByID)
-  .delete(deleteMenuItemByID);
+  .get(menuItemController.getMenuItemByID)
+  .patch(menuItemController.updateMenuItemByID)
+  .delete(menuItemController.deleteMenuItemByID);
 
 module.exports = router;

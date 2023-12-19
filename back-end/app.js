6a8632c7ddd356bf36c4b6471330779a85b59113
MuhammadA-C <phantomganhdi@gmail.com>
2023-12-18 17:03:12 -0800

    Working On REST API
    Working on creating the REST API.

--- back-end/app.js
+++ back-end/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mysql = require("mysql2");
 require("dotenv").config();
 
 const app = express();
@@ -16,5 +17,64 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
+
+// HTTP Methods //
+
+/*
+  API calls to include:
+    - Get all Menu Items
+    - Get all Ordered Items
+    - Get all Order Status
+
+    - Get a menu item by id
+    - Get an ordered item by id
+    - Get an order status by id
+
+    - Get all ordered items by order status/order id
+    - Get all order statuses by customer id (technically, don't need since customer id is the same for this version)
+*/
+
+// Create
+app.post("/api/v1/menu-items", (req, res) => {
+  res.status(201).json({
+    status: "success",
+    data: { test: "test" },
+  });
+});
+
+// Read
+app.get("/api/v1/menu-items", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: "insert number of objects",
+    data: { test: "test" },
+  });
+});
+
+// Read
+app.get("/api/v1/menu-items/:id", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: { test: "test" },
+  });
+});
+
+// Update
+app.put("/api/v1/menu-items", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: { test: "test" },
+  });
+});
+
+// Delete
+app.delete("/api/v1/menu-items/:id", (req, res) => {
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
+
+///////////////////
 
 app.listen(PORT);
